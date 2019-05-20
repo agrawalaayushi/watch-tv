feda912a38b2ca93a50c28d8ee316a73e9f87e5a
Manu <manu@Surabhis-MacBook-Pro-2.local>
2019-05-20 14:21:30 +0530

    video player UI updated

--- src/components/video-player/video-player.js
+++ src/components/video-player/video-player.js
@@ -1,32 +1,27 @@
 import React, { Component }  from 'react'
-import { POPEYEVIDEO, BOSKOS_VIDEO, MOOCHER_VIDEO } from '../../constants/images';
 
 class VideoPlayer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      videos: [
-        {
-          id: '1',
-          name: 'video-1',
-          src: {MOOCHER_VIDEO}
-        },
-        {
-          id: '2',
-          name: 'video-1',
-          src: {BOSKOS_VIDEO}
-        },
-        {
-          id: '3',
-          name: 'video-1',
-          src: {POPEYEVIDEO}
-        },
-      ],
-      videos1: [
-        MOOCHER_VIDEO,
-        BOSKOS_VIDEO,
-        POPEYEVIDEO
-      ]
+      // videos: [
+      //   {
+      //     id: '1',
+      //     name: 'video-1',
+      //     src: {MOOCHER_VIDEO}
+      //   },
+      //   {
+      //     id: '2',
+      //     name: 'video-1',
+      //     src: {BOSKOS_VIDEO}
+      //   },
+      //   {
+      //     id: '3',
+      //     name: 'video-1',
+      //     src: {POPEYEVIDEO}
+      //   },
+      // ],
+     
     }
   }
 
@@ -40,17 +35,14 @@ class VideoPlayer extends Component {
   // Lifecycle
   //-----------------------------------
 
-  
-  
 
   render() {
-    const {videos1}  = this.state
+    const {videos1}  = this.props
     return (
       <div className="player-view">
-        <video width="720" height="240" autoPlay className="player-video-wrapper" >
-            <source src={videos1[1]} type="video/mp4"/>
+        <video autoPlay className="player-video-wrapper" >
+            <source src={videos1[0]} type="video/mp4"/>
         </video>
-       
       </div>
     );
   }
